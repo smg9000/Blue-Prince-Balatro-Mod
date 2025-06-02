5e8a19c08c77c218b52d01dd2e8d42ffab0e80eb
Mathguy23 <165744246+Mathguy23@users.noreply.github.com>
2025-06-02 15:31:09 -0400

    github

---
+++ localization/en-us.lua
@@ -0,0 +1,56 @@
+return {
+    descriptions = {
+        Back = {
+
+        },
+        Other = {
+            
+        },
+        Enhanced = {
+
+        },
+        Joker = {
+
+        },
+        Spectral = {
+
+        }
+    },
+    misc = {
+        dictionary = {
+            b_experiments = "Experiments",
+        },
+        v_text = {
+            
+        },
+        v_dictionary = {
+        },
+        challenge_names = {
+
+        },
+        labels = {
+
+        },
+        ranks = {
+
+        },
+        suits_singular = {
+
+        },
+        suits_plural = {
+
+        },
+        poker_hands = {
+
+        },
+        poker_hand_descriptions = {
+
+        },
+        causes = {
+
+        },
+        effects = {
+
+        }
+    }
+}
